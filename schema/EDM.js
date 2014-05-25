a7f80756b0343fb791b1533ecbc7dc67b3d1c1cb
Tony <coolhzb@163.com>
2014-05-25 16:45:41 +0800

    update the context generation….

--- schema/EDM.js
+++ schema/EDM.js
@@ -12,32 +12,13 @@ exports.init = function(config, callback) {
 		logger.info('Total entity length: ' + entities.length);
 		entities.forEach(function(entity) {
 			logger.info('Entity Name: ' + entity.EntityName);
-			//var fullEntityName = 'odata.' + utils.trim(entity.EntityName);
-			var fullEntityName = utils.trim(entity.EntityName);
-			var key = entity.EntityName + 's';
+			var fullEntityName = utils.trim(entity.EntityName),
+			    key = entity.EntityName + 's',
+			    oentity = $data.Class.define(fullEntityName, $data.Entity, null, entity.Properties);
 
-			// Define the odata entity
-			var oentity = $data.Class.define(fullEntityName, $data.Entity, null, entity.Properties);
+			// Define the odata entity context
 			context[key] = {type: $data.EntitySet, elementType: oentity};
 		});
-
-    var node = $data.Class.define('Node',$data.Entity,null, {
-        "Name" : { type : "string",
-          key : true,
-          computed : false,
-          nullable : false },
-        "Count" : { type : "int" },
-        "Status" : { type : "int" }
-    });
-    context['Nodes'] =  {type: $data.EntitySet, elementType: node};
-
-    var link = $data.Class.define('Link',$data.Entity,null, {
-        "Source" : { type : "int" },
-        "Target" : { type : "int" },
-        "Jump" : { type : "int" },
-        "ReverseJump" : { type : "int" }
-    });
-    context['Links'] =  {type: $data.EntitySet, elementType: link};
 
 		callback(null, context);
 	});
